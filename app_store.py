20ab76ce96e9718d9d0da52cc55d608f713e4737
shiyong <shiyong@gaoyaya.com>
2023-04-04 14:43:32 +0800

    commit

--- app_store.py
+++ app_store.py
@@ -1,2 +1,6 @@
-import selenium
+from selenium import webdriver
+# from common.basepage import Base_page
+driver = webdriver.Chrome()
+# base = Base_page(driver)
+driver.get('https://www.baidu.com/')
 
